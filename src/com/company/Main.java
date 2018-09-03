998f472bf42f9bb5541d7e4c7ca4cd14d209a9a0
bacharuddinadieb <adieb.co.id@gmail.com>
2018-09-03 08:55:15 +0700

    Version 1.0

---
+++ src/com/company/Main.java
@@ -0,0 +1,34 @@
+package com.company;
+
+import java.util.Scanner;
+
+/**
+ * Aplikasi kalkulator penghitung segiduabelas
+ *
+ * @Author bacharuddinadieb
+ * @NIM 6706180113
+ * @Version 1.0
+ * @Since 2018-08-31
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        double l, k; // l = Luas, k = Keliling
+        double s = 4; //s = Sisi
+
+        System.out.println("\nKalkulator Segiduabelas \n");
+
+        Scanner InputUser = new Scanner(System.in);
+        System.out.print("Masukkan Sisi dari segi Duabelas: ");
+        s = InputUser.nextDouble();
+
+        k = s * 12;
+        l = 3 * (s * s) / (2 - Math.sqrt(3));
+
+        System.out.println("\n\nSisi Segiduabelas: " + s);
+        System.out.println("Keliling Segiduabelas: " + k);
+        System.out.println("Luas Segiduabelas: " + l);
+    }
+}
