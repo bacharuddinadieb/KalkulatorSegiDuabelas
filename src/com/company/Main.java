0998e203a3ca1a16009786624584604e2798275c
bacharuddinadieb <adieb.co.id@gmail.com>
2018-09-03 10:06:57 +0700

    Version 3.0

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -16,16 +16,16 @@ public class Main {
     public static void main(String[] args) {
 
         double l, k; // l = Luas, k = Keliling
-        double s = 4; //s = Sisi
+        double s = 4; // s = Sisi
 
         System.out.println("\nKalkulator Segiduabelas \n");
 
-        Scanner InputUser = new Scanner(System.in); //input user
+        Scanner InputUser = new Scanner(System.in); // Inisialisasi Scanner(input user)
         System.out.print("Masukkan Sisi dari segi Duabelas: ");
-        s = InputUser.nextDouble();
+        s = InputUser.nextDouble(); // Memasukkan input user ke variable
 
-        k = s * 12;
-        l = 3 * (s * s) / (2 - Math.sqrt(3));
+        k = s * 12; //Rumus Keliling
+        l = 3 * (s * s) / (2 - Math.sqrt(3)); //Rumus Luas
 
         System.out.println("\n\nSisi Segiduabelas: " + s);
         System.out.println("Keliling Segiduabelas: " + k);
